fe5f1d18f6013980ee18c8823a27d8e90359b78f
Midhun-Nk <midhunnk2021@gmail.com>
2026-05-16 16:25:31 +0530

    Updated

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "placehold.co" },
